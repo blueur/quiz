efa0ddfc51634094cbc9bc38f1289e48f9ea8ce7
David Tang <dev@blueur.com>
2024-11-02 10:42:20 +0100

    feat: add main.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/main.css'
 
+import 'bootstrap'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
