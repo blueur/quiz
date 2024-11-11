a9b468d95656416fd97ac5d95bbe15de7969fb7a
David Tang <dev@blueur.com>
2024-11-11 22:46:51 +0100

    feat: add QuestionRadio

--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import QuestionRadio from '@/components/QuestionRadio.vue'
 import { computed, ref } from 'vue'
 
 const cheval = ref<string | null>(null)
@@ -9,6 +10,7 @@ const filled = computed<boolean>(
 
 function submit(event: Event): void {
   event.preventDefault()
+
   let score: number = 0
   if (cheval.value === 'blanc') {
     score += 1
@@ -28,74 +30,26 @@ function reset(event: Event): void {
 
 <template>
   <form>
-    De quelle couleur est le cheval blanc de Napoléon ?
-    <div class="form-check">
-      <input
-        id="chevalBlanc"
-        v-model="cheval"
-        class="form-check-input"
-        type="radio"
-        name="cheval"
-        value="blanc"
-      />
-      <label class="form-check-label" for="chevalBlanc">Blanc</label>
-    </div>
-    <div class="form-check">
-      <input
-        id="chevalBrun"
-        v-model="cheval"
-        class="form-check-input"
-        type="radio"
-        name="cheval"
-        value="brun"
-      />
-      <label class="form-check-label" for="chevalBrun">Brun</label>
-    </div>
-    <div class="form-check">
-      <input
-        id="chevalNoir"
-        v-model="cheval"
-        class="form-check-input"
-        type="radio"
-        name="cheval"
-        value="noir"
-      />
-      <label class="form-check-label" for="chevalNoir">Noir</label>
-    </div>
-    Combien de pattes a un chat ?
-    <div class="form-check">
-      <input
-        id="chat2"
-        v-model="chat"
-        class="form-check-input"
-        type="radio"
-        name="chat"
-        value="2"
-      />
-      <label class="form-check-label" for="chat2">Deux</label>
-    </div>
-    <div class="form-check">
-      <input
-        id="chat4"
-        v-model="chat"
-        class="form-check-input"
-        type="radio"
-        name="chat"
-        value="4"
-      />
-      <label class="form-check-label" for="chat4">Quatre</label>
-    </div>
-    <div class="form-check">
-      <input
-        id="chat6"
-        v-model="chat"
-        class="form-check-input"
-        type="radio"
-        name="chat"
-        value="6"
-      />
-      <label class="form-check-label" for="chat6">Six</label>
-    </div>
+    <QuestionRadio
+      v-model="cheval"
+      text="De quelle couleur est le cheval blanc de Napoléon ?"
+      name="cheval"
+      :options="[
+        { name: 'blanc', text: 'Blanc' },
+        { name: 'brun', text: 'Brun' },
+        { name: 'noir', text: 'Noir' },
+      ]"
+    />
+    <QuestionRadio
+      v-model="chat"
+      text="Combien de pattes a un chat ?"
+      name="chat"
+      :options="[
+        { name: '2', text: 'Deux' },
+        { name: '4', text: 'Quatre' },
+        { name: '6', text: 'Six' },
+      ]"
+    />
     <button
       class="btn btn-primary"
       :class="{ disabled: !filled }"
